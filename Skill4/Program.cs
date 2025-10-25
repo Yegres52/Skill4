6e8cc8150f6d291b0aeb42c48db8512b65b42f20
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-25 22:58:33 +0300

    4.1.5 работа с логикой

--- Skill4/Program.cs
+++ Skill4/Program.cs
@@ -4,10 +4,12 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        var A = "A";
-        var B = "B";
+        var A = 1;
+        var B = 2;
+        double X = 5;
+        double Y = 4;
 
-        var result = A != B;
+        var result = (A < B) | (X > Y);
 
         Console.WriteLine(result);
         Console.ReadKey();
