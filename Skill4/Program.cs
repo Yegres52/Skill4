f95f11ad34e6ba143f7f625da454790f68a0c7b1
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-25 22:51:19 +0300

    Добавьте файлы проекта.

---
+++ Skill4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var myapples = 5;
+        var hisapples = 6;
+        var hispeares = 5;
+
+        var result = (myapples != hispeares) & (myapples < hisapples);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
