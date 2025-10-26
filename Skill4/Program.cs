b6948f22aa04d61f92ca62324b447f8b7095fd62
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-26 11:32:33 +0300

    Массивы

--- Skill4/Program.cs
+++ Skill4/Program.cs
@@ -1,38 +1,19 @@
 ﻿using System;
-using static System.Net.Mime.MediaTypeNames;
 
 class MainClass
 {
     public static void Main(string[] args)
     {
-        for (int i = 1; i < 5; i++)
-        {
-            var text = Console.ReadLine();
-            Console.WriteLine("Iteration {0}", i);
-            switch (text)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
 
-                    Console.WriteLine("Your color is red!");
-                    break;
+        //Размерность массива 
+        var arr = new int[] { 1, 2, 3, 4 };
+        var l = arr.Length;
 
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
+        //Разные виды инициализации массива
+        int[] arr1 = new int[4] { 1, 2, 3, 5 };
+        int[] arr2 = new int[] { 1, 2, 3, 5 };
+        int[] arr3 = new[] { 1, 2, 3, 5 };
+        int[] arr4 = { 1, 2, 3, 5 };
 
-                    Console.WriteLine("Your color is green!");
-                    break;
-
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    continue;
-            }
-        }
+    }
 }
